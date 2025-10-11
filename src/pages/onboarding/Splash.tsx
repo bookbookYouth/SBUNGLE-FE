3a1e1ba40d50f954385ee49e310ecafe30be2eb2
sujinRo <shtnwls1111@ewhain.net>
2025-10-12 01:03:42 +0900

    SGBG-19 style: 컴포넌트 변경으로 인한 splash 페이지 스타일 수정

--- src/pages/onboarding/Splash.tsx
+++ src/pages/onboarding/Splash.tsx
@@ -5,7 +5,7 @@ import logoIcon from '@/assets/logo.png';
 
 function SplashPage() {
   return (
-    <Flex direction="column" gap="12px" height="100vh" justifyContent="center">
+    <Flex direction="column" gap="12px" height="100%" width="100%" alignItems="center" justifyContent="center">
       <img src={logoIcon} alt="blur" height={'120px'} />
       <Txt typo="subTitle_sm_bold">독립서점 연계 블라인드 북 추천 서비스</Txt>
     </Flex>
