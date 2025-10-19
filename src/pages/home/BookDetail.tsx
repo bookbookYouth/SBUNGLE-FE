a8d3c4e7e8b30932d0648444f657579c59ef1982
sujinRo <shtnwls1111@ewhain.net>
2025-10-20 00:31:23 +0900

    SGBG-19 feat: tab 컴포넌트 스타일 수정 및 bookDetailPage에 추가

--- src/pages/home/BookDetail.tsx
+++ src/pages/home/BookDetail.tsx
@@ -1,5 +1,8 @@
+import { useState } from 'react';
 import { useParams } from 'react-router-dom';
 
+import { Spacing } from '@/components/base/Spacing';
+import { Tab } from '@/components/base/Tab';
 import { Header } from '@/components/common/Header';
 import { ImgCard } from '@/components/home/ImgCard';
 import { type Book, bookData } from '@/mock/bookData';
@@ -8,10 +11,21 @@ function BookDetailPage() {
   const params = useParams();
   const bookDetailData: Book | undefined = bookData.find((item) => item.id === params.id);
 
+  const [clickedTabItem, setClickedTabItem] = useState<'detail' | 'review'>('detail');
+
   return (
     <>
       <Header isBack isCart />
       <ImgCard genre={bookDetailData?.genre || ''} title={bookDetailData?.title || ''} />
+      <Spacing height="20px" />
+      <Tab>
+        <Tab.Item onClick={() => setClickedTabItem('detail')} active={clickedTabItem === 'detail'}>
+          상세 정보
+        </Tab.Item>
+        <Tab.Item onClick={() => setClickedTabItem('review')} active={clickedTabItem === 'review'}>
+          리뷰
+        </Tab.Item>
+      </Tab>
     </>
   );
 }
--- src/styles/base/tab.css.ts
+++ src/styles/base/tab.css.ts
@@ -3,10 +3,18 @@ import { style } from '@vanilla-extract/css';
 import { paletteTheme } from '../theme.css';
 
 export const tabStyle = style({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  width: '100%',
   border: 'none',
   borderBottom: `1px solid ${paletteTheme.palette.gray200}`,
+});
+
+export const tabItemStyle = style({
+  border: 'none',
   selectors: {
-    '&:hover, &:active': {
+    '&:hover, &:active, &[data-active="true"]': {
       borderBottom: `2px solid ${paletteTheme.palette.black}`,
       fontWeight: 'bold',
       color: paletteTheme.palette.black,
