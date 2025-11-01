323f8295911b0a5141e5099da35a55eb6a9f722f
sujinRo <shtnwls1111@ewhain.net>
2025-11-02 01:32:58 +0900

    SGBG-31 style: border 추가

--- src/components/bookstore/BookStory/index.tsx
+++ src/components/bookstore/BookStory/index.tsx
@@ -1,6 +1,8 @@
 import { Flex } from '@/components/base/Flex';
 import { Txt } from '@/components/base/Txt';
 
+import { paletteTheme } from '@/styles/theme.css';
+
 interface BookStoryProps {
   name: string;
   src: string;
@@ -16,7 +18,13 @@ export default function BookStory({ name, src }: BookStoryProps) {
       gap="8px"
       style={{ minWidth: '88px' }}
     >
-      <img src={src} alt={name} width={84} height={84} style={{ borderRadius: '50%', objectFit: 'cover' }} />
+      <img
+        src={src}
+        alt={name}
+        width={84}
+        height={84}
+        style={{ borderRadius: '50%', objectFit: 'cover', border: `1px solid ${paletteTheme.palette.black}` }}
+      />
       <Txt typo="point_sm">{name}</Txt>
     </Flex>
   );
--- src/pages/bookstore/index.tsx
+++ src/pages/bookstore/index.tsx
@@ -5,18 +5,25 @@ import BookStory from '@/components/bookstore/BookStory';
 import { Header } from '@/components/common/Header';
 import { bookstoreData } from '@/mock/bookStoreData';
 
+import { paletteTheme } from '@/styles/theme.css';
+
 function BookstorePage() {
   return (
     <>
       <Header title="독립서점" isCart />
-      <Flex height="48px" alignItems="center" style={{ padding: '0 20px' }}>
-        <Txt typo="subTitle_bold">인기 독립서점 best 5</Txt>
+      <Flex
+        direction="column"
+        style={{ borderBottom: `1px solid ${paletteTheme.palette.gray300}`, paddingBottom: '40px' }}
+      >
+        <Flex height="48px" alignItems="center" style={{ padding: '0 20px' }}>
+          <Txt typo="subTitle_bold">인기 독립서점 best 5</Txt>
+        </Flex>
+        <Slider>
+          {bookstoreData.best5.map((item) => (
+            <BookStory key={item.name} name={item.name} src={item.imageUrl} />
+          ))}
+        </Slider>
       </Flex>
-      <Slider>
-        {bookstoreData.best5.map((item) => (
-          <BookStory key={item.name} name={item.name} src={item.imageUrl} />
-        ))}
-      </Slider>
     </>
   );
 }
