c86af7401fbda9a4f28d5fb0cdd7f32ceceabda0
sujinRo <shtnwls1111@ewhain.net>
2025-11-02 00:34:49 +0900

    SGBG-31 feat: mockData 추가 및 best5 bookStore 페이지에 추가

--- src/pages/bookstore/index.tsx
+++ src/pages/bookstore/index.tsx
@@ -1,5 +1,24 @@
+import { Flex } from '@/components/base/Flex';
+import { Slider } from '@/components/base/Slider';
+import { Txt } from '@/components/base/Txt';
+import BookStory from '@/components/bookstore/BookStory';
+import { Header } from '@/components/common/Header';
+import { bookstoreData } from '@/mock/bookStoreData';
+
 function BookstorePage() {
-  return <></>;
+  return (
+    <>
+      <Header title="독립서점" isCart />
+      <Flex height="48px" alignItems="center" style={{ padding: '0 20px' }}>
+        <Txt typo="subTitle_bold">인기 독립서점 best 5</Txt>
+      </Flex>
+      <Slider>
+        {bookstoreData.best5.map((item) => (
+          <BookStory key={item.name} name={item.name} src={item.imageUrl} />
+        ))}
+      </Slider>
+    </>
+  );
 }
 
 export default BookstorePage;
