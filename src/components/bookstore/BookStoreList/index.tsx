4b5bb9e2b1f1e9428e57655d4e902d88cf0484d3
sujinRo <shtnwls1111@ewhain.net>
2025-11-03 20:07:19 +0900

    SGBG-31 feat: bookStoreList 컴포넌트 추가

---
+++ src/components/bookstore/BookStoreList/index.tsx
@@ -0,0 +1,46 @@
+import type React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { Flex } from '@/components/base/Flex';
+import { Grid } from '@/components/base/Grid';
+import { Txt } from '@/components/base/Txt';
+import { Card } from '@/components/common/Card';
+import { ROUTES } from '@/config/appConfig';
+import type { BookStore } from '@/mock/bookStoreData';
+
+interface BookStoreListProps {
+  title: React.ReactNode;
+  bookStoreData: BookStore[];
+}
+
+export const BookStoreList = ({ title, bookStoreData }: BookStoreListProps) => {
+  const navigate = useNavigate();
+
+  return (
+    <Flex direction="column">
+      <Flex height="48px" alignItems="center">
+        {title}
+      </Flex>
+      <Grid colGap="2px" rowGap="20px">
+        {bookStoreData.map((bookStore) => (
+          <Grid.Col key={bookStore.name}>
+            <Card
+              like
+              liked={bookStore.liked}
+              top={<img src={bookStore.images[0]} alt={bookStore.name} style={{ objectFit: 'cover' }} />}
+              bottom={
+                <Flex direction="column">
+                  <Txt typo="point_sm_bold">{bookStore.name}</Txt>
+                  <Txt typo="content_sm" color="gray300">
+                    {bookStore.address}
+                  </Txt>
+                </Flex>
+              }
+              onClick={() => navigate(ROUTES.bookStoreDetail.link(bookStore.name))}
+            />
+          </Grid.Col>
+        ))}
+      </Grid>
+    </Flex>
+  );
+};
